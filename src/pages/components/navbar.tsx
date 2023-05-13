33f404c6e497b8a1c88717a04e9b9113229a0355
EarthlyEric <minecarft.creeper.eric@gmail.com>
2023-05-13 21:56:49 +0800

    ✨: first commit.

---
+++ src/pages/components/navbar.tsx
@@ -0,0 +1,11 @@
+import { Component } from "react";
+
+export default function Navbar(){
+    return (
+        <div className="bg-black border-white">
+            <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+                
+            </div>
+        </div>
+    )
+}
