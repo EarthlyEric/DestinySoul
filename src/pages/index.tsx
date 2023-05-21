47ba7055cb6fae6c26b9332765b1747e13951742
EarthlyEric <minecarft.creeper.eric@gmail.com>
2023-05-21 15:51:26 +0800

    update title.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,6 +9,16 @@ export default function Home() {
         <meta name="description" content="Destiny‧Soul工作室官方網站 !"/>
       </Head>
       <Navbar></Navbar>
+      <section id="#home"className="bg-[url('/img/bg.png')] w-full h-screen bg-left bg-cover bg-dunes" >
+        <div className="px-2 py-5">
+          hi
+        </div>
+      </section>
+      <section id="#home"className="bg-white w-full h-screen bg-left bg-cover bg-dunes" >
+        <div className="px-2 py-5">
+          hi
+        </div>
+      </section>
     </div> 
   )
 }
