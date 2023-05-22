c393780bcebfe14aef669565a9a58d6ce83b1888
EarthlyEric <minecarft.creeper.eric@gmail.com>
2023-05-22 23:56:48 +0800

    ✨feature : change font type

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,11 +11,11 @@ export default function Home() {
       </Head>
       <Navbar></Navbar>
       <section id="#home"className=" w-full h-screen flex justify-center items-center bg-[url('/resources/img/bg.png')] bg-left bg-cover bg-dunes" >
-        <div className="px-10 grid grid-cols-1 gap-4 place-content-center h-48 text-center rounded-xl backdrop-blur-sm bg-white/30 ">
+        <div className="px-10 grid grid-cols-1 gap-4 place-content-center h-48 text-center rounded-xl backdrop-blur-sm bg-white/30">
           <div>
             <h1 className="text-6xl font-bold">Destiny‧Soul</h1>
           </div>
-          <div>
+          <div className="font-bold text">
             <TypewriterComponent  options={{
               autoStart: true,
               loop: true,
