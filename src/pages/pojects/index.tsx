854794893816c8624616db7b9ad4b355e15d061a
EarthlyEric <minecarft.creeper.eric@gmail.com>
2023-05-27 23:24:28 +0800

    add /pojects

---
+++ src/pages/pojects/index.tsx
@@ -0,0 +1,11 @@
+import Navbar from "@/components/navbar"
+import Title from "@/components/title"
+
+export default function Pojects() {
+    return(
+        <div>
+            <Navbar/>
+            <Title title="Pojects"/>
+        </div>
+    )
+}
