b987b868f82d44c7e5094b3053ee116cf077efcf
EarthlyEric <minecarft.creeper.eric@gmail.com>
2023-05-27 23:24:50 +0800

    add /pojects/gallery

---
+++ src/pages/pojects/gallery/index.tsx
@@ -0,0 +1,11 @@
+import Navbar from "@/components/navbar"
+import Title from "@/components/title"
+
+export default function Gallery() {
+    return(
+        <div>
+            <Navbar/>
+            <Title title="Gallery"/>
+        </div>
+    )
+}
